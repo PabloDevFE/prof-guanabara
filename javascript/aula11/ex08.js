1eeef8e6752339a81e0d9501c8e1c8b55254d3f0
Pablo <125980185+PabloDevFE@users.noreply.github.com>
2023-04-25 12:03:59 -0300

    fazendo exs

---
+++ javascript/aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 6 
+console.log(`a velocidade do seu carro é ${vel}Km/h`)
+if (vel >= 60) {
+    console.log('Você foi multado por estát muito rápido')
+}
+console.log('dirija sempre usando cinto de segurança')
---
+++ javascript/aula12/ex11.js
@@ -0,0 +1,27 @@
+var idade = 68
+if (idade < 16 ) {
+    console.log('Não vota')
+} else if (idade >= 16 && idade < 18 || idade > 65) {
+        console.log('Voto opicional')
+} else {
+            console.log('Voto obrigatório')
+}
+ 
+
+
+/*
+var idade = 80
+if (idade < 16 ) {
+    console.log('Não vota')
+} else {
+    if (idade >= 16 && idade < 18) {
+        console.log('Voto opicional')
+    } else {
+        if (idade >= 60) {
+            console.log('Voto facultatovo')
+        } else {
+            console.log('Voto obrigatório')
+        }
+    }
+}
+*/
