ca698fb0aa41a534e1c49f5890f3db0753e90568
Pablo <125980185+PabloDevFE@users.noreply.github.com>
2023-04-27 09:19:54 -0300

    resolvendo exercicio

--- javascript/exercicios/ex1/script.js
+++ javascript/exercicios/ex1/script.js
@@ -1,1 +1,22 @@
- 
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `<h2>Agora são exatamente ${hora} horas</h2>`
+    if (hora > 4 && hora < 12) {
+        //BOM DIA
+        msg.innerHTML += '<p>Bom dia!</p>'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE
+        img.src = 'tarde.jpg'
+        document.body.style.backgroundColor = '#e79243'
+        msg.innerHTML += '<p>Boa tarde!</p>'
+    } else {
+        //BOA NOITE
+        img.src = 'noite.jpg'
+        document.body.style.backgroundColor = '#0a0620'
+        msg.innerHTML += '<p>Boa noite!</p>'
+    }
+}
