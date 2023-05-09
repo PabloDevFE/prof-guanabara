acd0c4585271fd2817c3cbbd61fc8c5f81cff5a8
Pablo <125980185+PabloDevFE@users.noreply.github.com>
2023-05-09 10:03:21 -0300

    correção

--- javascript/exercicios/ex3/script.js
+++ javascript/exercicios/ex3/script.js
@@ -1,15 +1,15 @@
 function contar() {
-    
-    
-    var ini = document.getElementById('#iinicio')
-    var fim = document.getElementById('#ifim')
-    var passo = document.getElementById('#ipasso')
-    var res = document.getElementById('#res')
-    var inicio = Number(ini.value)
-    var final = Number(fim.value)
-    var pulo = Number(passo.value)
+    var ini = document.getElementById('inicio');
+    var fim = document.getElementById('fim');
+    var passo = document.getElementById('passo');
+    var res = document.getElementById('res');
+    var inicio = Number(ini.value);
+    var final = Number(fim.value);
+    var pulo = Number(passo.value);
+
+    res.innerHTML = ""; // Limpa o conteúdo anterior
 
     for (var i = inicio; i <= final; i += pulo) {
-        res.innerHTML += i + ' '
+        res.innerHTML += ` \u{1F449} ${i}`;
     }
-} 
+}
