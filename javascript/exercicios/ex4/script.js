8cdb137ff8f155b8cf3f623d71c4c610da091c64
Pablo <125980185+PabloDevFE@users.noreply.github.com>
2023-05-09 11:29:09 -0300

    exercicio

--- javascript/exercicios/ex4/script.js
+++ javascript/exercicios/ex4/script.js
@@ -1,1 +1,19 @@
- 
+function tabuada() {
+    var num = document.getElementById('inum')
+    var tab = document.getElementById('sel')
+
+    if (num.value.length == 0) {
+        window.alert('Digite um numero valido')
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+
+        while (c <= 10){
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+} 
